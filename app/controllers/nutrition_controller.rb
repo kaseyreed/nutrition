93ee13d6c522a14de0e6aedc64d33dc67eebbc1b
Kasey Reed <charles.kasey.reed@gmail.com>
2022-05-14 18:07:29 -0500

    group by date

--- app/controllers/nutrition_controller.rb
+++ app/controllers/nutrition_controller.rb
@@ -1,6 +1,7 @@
 class NutritionController < ApplicationController
   def index
-    @entries = NutritionEntry.order(consumed_at: :desc).all
+    # @entries = NutritionEntry.order(consumed_at: :desc).all
+    @entries_map = NutritionEntry.order(consumed_at: :desc).all.group_by { |e| e.consumed_at.to_date }
     @training_entries = TrainingEntry.order(created_at: :desc).all
   end
 
@@ -8,5 +9,7 @@ class NutritionController < ApplicationController
     consumed_at = Time.parse("#{params[:date]} #{params[:time]}")
 
     NutritionEntry.create!(entry: params[:entry], consumed_at:,)
+
+    redirect_to action: 'index'
   end
 end
